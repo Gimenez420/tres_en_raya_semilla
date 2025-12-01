b1539fa75f88b14b908345f429c85794e8075e43
Gimenez.SSJ <ricardogimenezvalero01@gmail.com>
2025-12-01 15:54:46 +0100

    Update utilidades.py

--- utilidades.py
+++ utilidades.py
@@ -4,14 +4,20 @@ def comprobar_ganador(x, y, tablero):
     jugador = tablero[x][y]
     print("Voy a comprobar ", jugador, " en ", x, ",", y)
 
+    if jugador == '-':
+        return False
+    
+    tamanyo = len(tablero)
+
     ##########################################
     # Miramos hacia la izquierda y derecha
-    victoria_lateral = True
+
+    victoria_fila = True
 
     # TODO: comprobacion
     pass
     
-    if victoria_lateral:
+    if victoria_fila:
         return True
     ##########################################
     # Miramos hacia abajo
@@ -39,7 +45,16 @@ def mostrar_tablero(tablero):
 # - --> 0
 def convertir_tablero(tablero):
     nuevo_tablero = []
-
+    for linea in tablero:
+        lista = []
+        for char in linea:
+            if char == 'X':
+                lista.append(1)
+            elif char == 'O':
+                lista.append(-1)
+            else:
+                lista.append(0)
+        nuevo_tablero.append(lista)
     ##########################################
     #TODO: Convertirlo a una lista de listas
     # ej: [[1,1,0], [1,-1,-1], [0, 0, 0]]
